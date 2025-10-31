08b3a302efea24409397f6a3c2613c360839afc9
Rommy <rommygobardhanv@gmail.com>
2025-10-31 16:06:02 +0100

    setup initial poetry configuration

--- main.py
+++
@@ -1,5 +0,0 @@
-# TODO:Starting with a clear file from scratch
-# - Create WaterModel class containing core functions from the run method()
-# - Create DatasetIO class containing the main read and write functions for the input data and final simulation results
-# - Check where to incorporate the application configuration
-# - Evaluate where the locate each class
